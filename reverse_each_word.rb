ec4102f8f1e96286877c42e5e0d19414bc0f3e0e
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-15 15:51:35 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do
+    |i| new_array << i.reverse
+  end
+  new_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  array.collect do
+    |i| new_array << i.reverse
+  end
+  new_array.join(" ")
+end
