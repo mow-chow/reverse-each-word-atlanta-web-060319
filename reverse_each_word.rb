9e4268024fccbfbf05ffc1d033b13ae17dd0cc40
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-15 15:49:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each do
-    |i| new_array << string.reverse
+    |i| new_array << i.reverse
   end
   new_array.join(" ")
 end
